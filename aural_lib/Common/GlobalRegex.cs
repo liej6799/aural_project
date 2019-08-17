1638548b0874fc72f438b57b496d6fe6a001a276
Joes Lie <liej6799@protonmail.com>
2019-08-17 22:06:51 +0800

    added common lib for business logic.

---
+++ aural_lib/Common/GlobalRegex.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aural_lib.Common
+{
+    public class GlobalRegex
+    {
+        public static string PasswordRegex
+        {
+            get { return Properties.Resources.password_regex; }
+        }
+    }
+}
---
+++ aural_model_lib/User/UserApiModel.cs
@@ -0,0 +1,40 @@
+﻿using aural_lib.Common;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace aural_model_lib.User
+{
+    public class UserApiModel
+    {
+        public class UserApiRegisterModel
+        {
+            // Id is not required as it will be
+            // generated automatically
+            public string Id { get; set; }
+            [Required]
+            public string Username { get; set; }
+            [Required]
+            public string Password { get; set; }
+
+            public void Validation()
+            {
+                // Validate the data provided earlier
+                
+            }
+        }
+
+        // Second Validatiion
+        public class UserApiRegisterModelValidation : AbstractValidator<UserApiRegisterModel>
+        {
+            public UserApiRegisterModelValidation()
+            {
+                RuleFor(username => username.Username).NotNull();
+                RuleFor(password => password.Password).NotNull()
+                    .MinimumLength(8).WithErrorCode(Properties.Resources.user_api_password_less_character)
+                    .Matches(GlobalRegex.PasswordRegex).WithErrorCode(Properties.Resources.user_api_password_not_strong);
+            }
+        }
+    }
+}
